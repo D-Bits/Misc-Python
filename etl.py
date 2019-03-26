153089adf1a7801327e91a588af2c3c743def90d
Dana <lockwood.da88@gmail.com>
2019-03-26 14:10:06 -0700

    Commited ETL Tool

---
+++ etl.py
@@ -0,0 +1,17 @@
+import csv, sqlalchemy as sql
+from sqlalchemy import create_engine
+import psycopg2
+
+# Define connection
+engine = create_engine("postgresql+psycopg2://postgres:bVx2$U47S4@!mTpn%qm4@/ETL_Test")
+
+# Seed the users table w/ sample data from csv file
+with open('data.csv', 'r') as data_file:
+    reader = csv.reader(data_file)
+    next(reader) # Skip header row
+    for row in reader:
+        engine.execute( # Parameterize query to avoid SQL injections
+            "INSERT INTO users(email, lname, fname, homeaddress) VALUES(%s, %s, %s, %s)",
+            row
+        )
+
